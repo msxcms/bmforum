c9e5ce34c9cf36f9efa89b8b3193b6041bee2f24
ruinjoy <ruinjoy@163.com>
2018-09-01 10:40:44 +0800

    兼容7.2，修复错误

--- bmb/lang/eng/post_global.php
+++ bmb/lang/eng/post_global.php
@@ -5,13 +5,13 @@ $war[1]="<center><p><p>The moderator of this forum has banned your permission of
 $errc[0]="An Error Has Occurred";
 $errc[1]="Forum not found.";
 $errc[2]="The forum you request does not exist.";
-$errc[3]="";
+$errc[3]=array();
 $errc[4]="Too soon to post";
 $errc[5]="You cannot post a thread until you have been logged in for at least {$logon_post_second} seconds, now you have only logged in for {$onlylps} seconds.";
 $errc[6]="Error: Log in incorrect.";
 $errc[7]="Sorry, this username has been occupied.";
 $errc[8]="You have no permission of posting in this forum.";
-$errc[ab9]="You can not post in a recycling bin.";
+$errc['ab9']="You can not post in a recycling bin.";
 $errc[9]="An Error Has Occurred: ";
 $errc[10]="Back to previous page";
 $errc[11]="You can publish a new post after $reg_posting hours since you have registered.";
@@ -41,11 +41,11 @@ $send_HTML_mail_lng[4]="You have replies~~~";
 $login_user[0]="Username: ";
 $login_user[1]="Password: ";
 $login_user[2]="Forget password?";
-$print_form[G1]="<strong>Tags: </strong>";
-$print_form[G2]="Tags is some keyword to the article¡£";
-$print_form[G3]="<a href='plugins.php?p=tags' target=_blank>[ List of Tags ]</a>";
-$print_form[G4]="Maximum of Tags: ";
-$print_form[tags]="You must set a tags";
+$print_form['G1']="<strong>Tags: </strong>";
+$print_form['G2']="Tags is some keyword to the article¡£";
+$print_form['G3']="<a href='plugins.php?p=tags' target=_blank>[ List of Tags ]</a>";
+$print_form['G4']="Maximum of Tags: ";
+$print_form['tags']="You must set a tags";
 $errc[12]="Your article contains too many forbidden keywords, so it will be censored.";
 
 
